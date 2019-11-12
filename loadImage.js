22ce70e5913eebf1753034f65b7c71dfddf52978
LenaChu <a87192611@gmail.com>
2019-11-11 20:01:59 -0600

    Corrected errors

---
+++ loadImage.js
@@ -0,0 +1,7 @@
+var searchParam = new URLSearchParams(window.location.search);
+var imageValue1 = searchParam.get("image1");
+var imageValue2 = searchParam.get("image2");
+document.getElementById("old-large").src = imageValue1;
+document.getElementById("new-large").src = imageValue2;
+document.getElementById("old-small").src = imageValue1;
+document.getElementById("new-small").src = imageValue2;
